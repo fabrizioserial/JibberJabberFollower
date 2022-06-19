318fd9f6458cad729673e156f668a94441672f9d
fabrizioserial <fabrizio.serial@ing.austral.edu.ar>
2022-06-19 10:52:22 -0300

    fix: is working this wea?

--- src/main/java/com/fabrizioserial/jibberjabberfollowerservice/repository/FollowRepository.java
+++ src/main/java/com/fabrizioserial/jibberjabberfollowerservice/repository/FollowRepository.java
@@ -1,14 +1,14 @@
 package com.fabrizioserial.jibberjabberfollowerservice.repository;
 
 import com.fabrizioserial.jibberjabberfollowerservice.entity.Follow;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.awt.print.Pageable;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FollowRepository extends PagingAndSortingRepository<Follow, UUID> {
-
-    List<Follow> findAllFollowers(double price, Pageable pageable);
 }
